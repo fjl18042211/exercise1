f4db80ca0b9fc9bda2dda0cb7663c40a1f062884
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-03-06 14:41:32 +0800

    ex1

---
+++ exercise1/源.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv.hpp>
+using namespace cv;
+int main()
+{
+	cv::Mat src_color = imread("D:\\3.jpg");
+	std::vector<cv::Mat>channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red", R);
+	cv::imshow("blue", B);
+	cv::imshow("green", G);
+	cv::imshow("orginal Mat", src_color);
+	waitKey(0);
+}
