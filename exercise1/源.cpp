dfb8239055bd5237b81c124fe180eeaf0ed6257d
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-03-27 14:17:19 +0800

    week5ass1

--- exercise1/源.cpp
+++ exercise1/源.cpp
@@ -6,23 +6,34 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	cv::Mat	M1;
-	cv::Mat	result1;
-	cv::Mat	result2;
-	cv::Mat	result3;
-	cv::Mat	result4;
-	cv::Mat srcMat = imread("D:\\4.png", 0);
-	cv::threshold(srcMat, M1, 0, 255, THRESH_OTSU);
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-	morphologyEx(srcMat, result1, MORPH_ERODE, kernel);
-	morphologyEx(srcMat, result2, MORPH_DILATE, kernel);
-	morphologyEx(srcMat, result3, MORPH_OPEN, kernel);
-	morphologyEx(srcMat, result4, MORPH_CLOSE, kernel);
-	imshow("result1", result1);
-	imshow("result2", result2);
-	imshow("result3", result3);
-	imshow("result4", result4);
-	waitKey(0);
+	VideoCapture cap;
+	cap.open(0);
+	if (!cap.isOpened())
+	{
+		std::cout << "不能打开视频文件" << std::endl;
+		return -1;
+	}
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat dst;
+        bool rSucess=cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else
+		{   
+			cv::medianBlur(frame, dst, 3);
+			cv::imshow("dst", dst);
+
+		}
+
+	}
+	waitKey(30);
 	return 0;
 
 }
